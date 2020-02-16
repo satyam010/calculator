9d3b2376a61621473825647a3030e00953aee1d6
Satyam Prakash <prakashsatyam6@gmail.com>
2020-02-16 14:46:56 +0530

    Updates

---
+++ precache-manifest.d8450f878eb10e19dcf9e12eba96f08e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f443b4dcd46e10b8af76a6ce659e3987",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "bc98d80696b6941e6896",
+    "url": "/calculator/static/css/main.1fd0987f.chunk.css"
+  },
+  {
+    "revision": "91437916b2e9c943958e",
+    "url": "/calculator/static/js/2.2c2680ee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator/static/js/2.2c2680ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc98d80696b6941e6896",
+    "url": "/calculator/static/js/main.b5d4186a.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
