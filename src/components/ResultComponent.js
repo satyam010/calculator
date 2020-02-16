621b6bbccaffb546894f4f2d29204437a7afc277
Satyam Prakash <prakashsatyam6@gmail.com>
2020-02-16 14:29:25 +0530

    initial commit

---
+++ src/components/ResultComponent.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class ResultComponent extends Component {
+
+
+    render() {
+        return (
+            <div className="result">
+                <p>{this.props.result}</p>
+            </div>
+    )
+        ;
+    }
+}
+
+
+export default ResultComponent;
+
